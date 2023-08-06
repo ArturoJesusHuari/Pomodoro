a8e7e9a6cead221b731a340fc90c9563e12830e5
ArturoJesusHuari <115654291+ArturoJesusHuari@users.noreply.github.com>
2023-08-06 14:19:58 -0500

    animated.01

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ def main():
         cronometro(listAlas,work,Fore.GREEN)
         cronometro(listRest,chill,Fore.RED)
         clear()
-        if(input('Continuar?: ')!=('Y'or'Yes'or'y'or'yes')):
+        if((input('Continuar?: ')) in ['Y','Yes','y','yes']):
             break
 if __name__=='__main__':
     main()
